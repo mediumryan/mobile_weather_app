2b0de06d59b6b0cda1d6d7e65c48118b3d38f677
Ryan <straightwind0726@gmail.com>
2023-07-19 15:58:44 +0900

    [Update] pagenation & scroll & Ionicons

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import styled from "styled-components/native";
 import moment from "moment";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Fontisto } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+  Dimensions,
+} from "react-native";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   const [ok, setOk] = useState(true);
@@ -43,18 +54,81 @@ export default function App() {
           <TopContainer>
             <City>{days ? days.name : "Loading..."}</City>
           </TopContainer>
-          <BottomContainer>
-            <Date>{currentDate}</Date>
-            <Temp>
-              {days
-                ? `${parseFloat(days.main.temp).toFixed(1)}°C`
-                : "Loading..."}
-            </Temp>
-            <Weather>{days ? days.weather[0].main : "Loading..."}</Weather>
-            <Description>
-              {days ? days.weather[0].description : "Loading..."}
-            </Description>
-          </BottomContainer>
+          <Horizontal />
+          <BottomBox horizontal pagingEnabled>
+            <BottomContainer>
+              <Date>{currentDate}</Date>
+              <Temp>
+                {days
+                  ? `${parseFloat(days.main.temp).toFixed(1)}°C`
+                  : "Loading..."}
+              </Temp>
+              <Fontisto
+                name="lightning"
+                size={48}
+                color="white"
+                style={{ marginLeft: 24, marginTop: 24 }}
+              />
+              <Weather>{days ? days.weather[0].main : "Loading..."}</Weather>
+              <Description>
+                {days ? days.weather[0].description : "Loading..."}
+              </Description>
+            </BottomContainer>
+            <BottomContainer>
+              <Date>{currentDate}</Date>
+              <Temp>
+                {days
+                  ? `${parseFloat(days.main.temp).toFixed(1)}°C`
+                  : "Loading..."}
+              </Temp>
+              <Fontisto
+                name="cloudy"
+                size={48}
+                color="white"
+                style={{ marginLeft: 24, marginTop: 24 }}
+              />
+              <Weather>{days ? days.weather[0].main : "Loading..."}</Weather>
+              <Description>
+                {days ? days.weather[0].description : "Loading..."}
+              </Description>
+            </BottomContainer>
+            <BottomContainer>
+              <Date>{currentDate}</Date>
+              <Temp>
+                {days
+                  ? `${parseFloat(days.main.temp).toFixed(1)}°C`
+                  : "Loading..."}
+              </Temp>
+              <Fontisto
+                name="day-sunny"
+                size={48}
+                color="white"
+                style={{ marginLeft: 24, marginTop: 24 }}
+              />
+              <Weather>{days ? days.weather[0].main : "Loading..."}</Weather>
+              <Description>
+                {days ? days.weather[0].description : "Loading..."}
+              </Description>
+            </BottomContainer>
+            <BottomContainer>
+              <Date>{currentDate}</Date>
+              <Temp>
+                {days
+                  ? `${parseFloat(days.main.temp).toFixed(1)}°C`
+                  : "Loading..."}
+              </Temp>
+              <Fontisto
+                name="rains"
+                size={48}
+                color="white"
+                style={{ marginLeft: 24, marginTop: 24 }}
+              />
+              <Weather>{days ? days.weather[0].main : "Loading..."}</Weather>
+              <Description>
+                {days ? days.weather[0].description : "Loading..."}
+              </Description>
+            </BottomContainer>
+          </BottomBox>
         </>
       )}
     </MainContainer>
@@ -70,36 +144,52 @@ const TopContainer = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: tomato;
 `;
 
 const City = styled.Text`
   font-size: 48px;
   font-weight: 600;
+  color: white;
+`;
+
+const Horizontal = styled.View`
+  width: 100%;
+  flex: 0.05;
+  background-color: white;
+`;
+
+const BottomBox = styled.ScrollView`
+  flex: 4;
 `;
 
 const BottomContainer = styled.View`
-  flex: 4;
-  align-items: center;
-  background-color: aquamarine;
+  width: ${SCREEN_WIDTH};
 `;
 
 const Date = styled.Text`
   font-size: 28px;
   margin-top: 32px;
+  margin-left: 24px;
+  color: white;
 `;
 
 const Temp = styled.Text`
-  font-size: 128px;
+  font-size: 72px;
   margin-top: 32px;
+  color: white;
+  margin-left: 24px;
 `;
 
 const Weather = styled.Text`
   font-size: 50px;
   margin-top: 28px;
+  color: white;
+  margin-left: 24px;
 `;
 
 const Description = styled.Text`
   font-size: 24px;
   margin-top: 14px;
+  color: white;
+  margin-left: 24px;
 `;
